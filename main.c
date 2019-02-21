153b533c4db96189ad138cefcce8dedafda47686
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-02-21 11:23:36 +0100

    fonction factorial 2

--- main.c
+++ main.c
@@ -3,25 +3,24 @@
 
 int main()
 {
-    int numero=0, i=0, resultat=0;
+    int numero=0, resultat=0;
     printf("Entrez un numero\n");
     scanf("%i", &numero);
-    resultat=numero;
-
-    for(i=1; i<numero; i++){
-    resultat=resultat*i;
-    }
-    printf("\n\nle calcul factorial de %i est %i\n\n", numero,resultat);
-
-     int numero=0, i=0, resultat=0;
-    printf("Entrez un numero\n");
-    scanf("%i", &numero);
-    resultat=numero;
-
-    for(i=1; i<numero; i++){
-    resultat=resultat*i;
-    }
-    printf("\n\nle calcul factorial de %i est %i\n\n", numero,resultat);
+    resultat=factorial(numero);
+    printf("\n\nle resultat factorial de %i est %i\n\n", numero,resultat);
 
     return 0;
+}
+
+int factorial (int numero){
+
+    int i, resultat;
+
+    resultat=numero;
+
+    for(i=1; i<numero; i++){
+        resultat=resultat*i;
+    }
+
+    return resultat;
 }
