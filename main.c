1601b629e80931c85168f480d90a53ceecff743d
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-02-21 10:31:36 +0100

    First comit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero=0, i=0, resultat=0;
+    printf("Entrez un numero\n");
+    scanf("%i", &numero);
+    resultat=numero;
+
+    for(i=1; i<numero; i++){
+    resultat=resultat*i;
+    }
+    printf("\nle calcul factorial de %i est %i\n\n", numero, resultat);
+    return 0;
+}
